cc7e2339767141a1ba6446aaf883206419b82e22
Ruan <ruan.ruanzinho777@gmail.com>
2025-07-14 23:35:10 -0300

    Criar ex11.py

---
+++ ex11.py
@@ -0,0 +1,14 @@
+int1 = int(input("Informe um numero inteiro: "))
+
+int2 = int(input("Informe outro numero inteiro: "))
+
+real = float(input("Informe um numero real: "))
+
+int1 = int1 * 2
+int2 = int2 / 2
+
+print(f"O dobro do primeiro numero é: {int1}\nE a metade do segundo é: {int2}")
+
+triplo1 = int1 / 2 * 3
+triplo = triplo1 + real * 3
+print(f"A soma do triplo do primeiro com o triplo do segundo é: {triplo}")
