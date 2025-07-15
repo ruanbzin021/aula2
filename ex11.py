d883eac24b02b931741734c489b3fc0e6527fdbb
Ruan <ruan.ruanzinho777@gmail.com>
2025-07-14 23:42:59 -0300

    Atualizar o ex11.py

--- ex11.py
+++ ex11.py
@@ -7,11 +7,11 @@ real = float(input("Informe um numero real: "))
 int1 = int1 * 2
 int2 = int2 / 2
 
-print(f"O dobro do primeiro numero é: {int1}\nE a metade do segundo é: {int2}")
+print(f"O dobro do primeiro numero é: {int1}\nA metade do segundo é: {int2}")
 
 triplo1 = int1 / 2 * 3
 triplo = triplo1 + real * 3
-print(f"A soma do triplo do primeiro com o triplo do segundo é: {triplo}")
+print(f"A soma do triplo do primeiro com o triplo do terceiro é: {triplo}")
 
 realc = real * real * real
 
