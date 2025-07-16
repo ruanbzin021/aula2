f3fc435f8254eeea1b66eff38106ebc9b08e37d0
Ruan <ruan.ruanzinho777@gmail.com>
2025-07-16 18:16:05 -0300

    Update ex8.py

--- ex8.py
+++ ex8.py
@@ -1,4 +1,4 @@
-valor_hora = float("Quanto vc ganha por hora? ")
-horas_trabalhadas = float("Quantas horas por dia? ")
+valor_hora = float(input("Quanto vc ganha por hora? "))
+horas_trabalhadas = float(input("Quantas horas por dia? "))
 salario_total = horas_trabalhadas * valor_hora
 print(f"O total do seu salário no mês é: R$ {salario_total:.2f}")
