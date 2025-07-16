54ae474296f94a7aa710fd865ccaa0f91d25edac
Ruan <ruan.ruanzinho777@gmail.com>
2025-07-16 18:17:40 -0300

    Update ex8.py

--- ex8.py
+++ ex8.py
@@ -1,4 +1,5 @@
-valor_hora = float(input("Quanto vc ganha por hora? "))
-horas_trabalhadas = float(input("Quantas horas por dia? "))
-salario_total = horas_trabalhadas * valor_hora
-print(f"O total do seu salário no mês é: R$ {salario_total:.2f}")
+salario_hora = float(input("Quanto vc ganha por hora? "))
+horas_dias = float(input("Quantas horas por dia? "))
+mes = horas_dias * 30 
+m2 = mes *salario_hora
+print(m2) 
